b3ce9cbebaadb5996d787c213a7da3a11ab1d0c8
Vishnuvarthan <131575256+Vishnu-Varthan-05@users.noreply.github.com>
2023-11-30 15:34:56 +0530

    -Initial commit and pust to github

---
+++ app.py
@@ -0,0 +1,129 @@
+from flask import Flask, jsonify, send_from_directory, request
+import mysql.connector
+from flask_cors import CORS
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+CORS(app)
+
+db_config = {
+    'host': '10.30.10.13',
+    'user': 'bestshop',
+    'password': 'bestshop',
+    'database': 'best_shop',
+}
+
+def get_db_connection():
+    return mysql.connector.connect(**db_config)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg', 'gif'}
+
+@app.route('/categories', methods=['GET', 'POST'])
+def get_all_categories():
+    if request.method == 'GET':
+        try:
+            connection = get_db_connection()
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute('SELECT * FROM category')
+            categories = cursor.fetchall()
+            return jsonify(categories)
+        except Exception as e:
+            return jsonify({'error': str(e)})
+        finally:
+            cursor.close()
+            connection.close()
+    
+    elif request.method == 'POST':
+        try:
+            data = request.form
+            category_name = data['category_name']
+
+            connection = get_db_connection()
+            cursor = connection.cursor()
+
+            cursor.execute('INSERT INTO category (category_name) VALUES (%s)', (category_name,))
+            connection.commit()
+
+            category_id = cursor.lastrowid
+
+            if 'image' in request.files:
+                file = request.files['image']
+                if file and allowed_file(file.filename):
+                    filename = f"{category_id}_{secure_filename(file.filename)}"
+                    fileToBeSaved = os.path.join(app.config['UPLOAD_FOLDER'], 'images', 'category', filename)
+                    file.save(fileToBeSaved)
+                    cursor.execute('UPDATE category SET category_image = %s WHERE category_id = %s', (fileToBeSaved, category_id))
+                    connection.commit()
+
+            return jsonify({'message': 'Category added successfully'})
+        except Exception as e:
+            app.logger.error(f"Error adding category: {str(e)}")
+            return jsonify({'error': 'Internal Server Error'}), 500
+        finally:
+            cursor.close()
+            connection.close()
+
+# @app.route('/category-fields', methods = ['GET'])
+# def get_all_category_fields():
+#     try:
+#         connection = get_db_connection()
+#         cursor = connection.cursor(dictionary = True)
+#         cursor.execute('SELECT * FROM category_fields')
+#         category_fields = cursor.fetchall()
+#         return jsonify(category_fields)
+#     except Exception as e:
+#         return jsonify({'error': str(e)})
+#     finally:
+#         cursor.close()
+#         connection.close()
+
+# @app.route('/fields-details', methods=['GET'])
+# def get_all_field_details():
+#     try:
+#         connection = get_db_connection()
+#         cursor = connection.cursor(dictionary = True)
+#         cursor.execute('SELECT * FROM field_details')
+#         field_details = cursor.fetchall()
+#         return jsonify(field_details)
+#     except Exception as e:
+#         return jsonify({'error': str(e)})
+#     finally:
+#         cursor.close()
+#         connection.close()
+
+@app.route('/categories/<int:categoryID>', methods=['GET'])
+def get_category_fields(categoryID):
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute('SELECT * FROM category_fields WHERE category_id = %s', (categoryID,))
+        category_fields = cursor.fetchall()
+        return jsonify(category_fields)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    finally:
+        cursor.close()
+        connection.close()
+
+@app.route('/categories/<int:categoryID>/<int:fieldID>', methods=['GET'])
+def get_field_details(categoryID, fieldID):
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute('SELECT * FROM field_details WHERE field_id = %s', (fieldID,))
+        field_details = cursor.fetchall()
+        return jsonify(field_details)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    finally:
+        cursor.close()
+        connection.close()
+
+@app.route('/uploads/<path:filename>', methods=['GET'])
+def uploaded_file(filename):
+    return send_from_directory('uploads', filename)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
