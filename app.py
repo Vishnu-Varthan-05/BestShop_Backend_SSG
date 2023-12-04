77bb0012d2caa1e9bebe0d80eaf62d54045edd97
Vishnuvarthan <131575256+Vishnu-Varthan-05@users.noreply.github.com>
2023-12-05 01:31:35 +0530

    -- Added "POST" for field_details -- Created add_field_details html

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ CORS(app)
 #     'password': 'bestshop',
 #     'database': 'best_shop',
 # }
+
 db_config = {
     'host': '127.0.0.1',
     'user': 'root',
@@ -110,19 +111,57 @@ def get_all_category_fields():
             app.logger.error(f"Error adding category field: {str(e)}")
             return jsonify({'error': 'Internal Server Error'}), 500 
 
-# @app.route('/fields-details', methods=['GET'])
-# def get_all_field_details():
-#     try:
-#         connection = get_db_connection()
-#         cursor = connection.cursor(dictionary = True)
-#         cursor.execute('SELECT * FROM field_details')
-#         field_details = cursor.fetchall()
-#         return jsonify(field_details)
-#     except Exception as e:
-#         return jsonify({'error': str(e)})
-#     finally:
-#         cursor.close()
-#         connection.close()
+@app.route('/field-details', methods=['GET', 'POST'])
+def manage_field_details():
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    if request.method == 'GET':
+        try:
+            cursor.execute('SELECT * FROM field_details')
+            field_details = cursor.fetchall()
+            return jsonify(field_details)
+        except mysql.connector.Error as e:
+            return jsonify({'error': str(e)}), 500
+        finally:
+            cursor.close()
+            connection.close()
+    
+    elif request.method == 'POST':
+        try:
+            data = request.form
+            field_name = data['field_name']
+            details_name = data['details_name']
+            connection = get_db_connection()
+            cursor = connection.cursor()
+            cursor.execute('SELECT field_id FROM category_fields WHERE field_name = %s', (field_name,))
+            field_id_tuple = cursor.fetchone()
+            if not field_id_tuple:
+                return jsonify({'error': 'Invalid field_name'}), 400
+            field_id = field_id_tuple[0]
+            cursor.execute(
+                'INSERT INTO field_details (field_id, details_name) VALUES (%s, %s)',
+                (field_id, details_name)
+            )
+            connection.commit()
+            field_details_id = cursor.lastrowid
+            if 'image' in request.files:
+                file = request.files['image']
+                if file and allowed_file(file.filename):
+                    filename = f"{field_details_id}_{secure_filename(file.filename)}"
+                    file_to_be_saved = os.path.join(app.config['UPLOAD_FOLDER'], 'images', 'field_details', filename)
+                    file.save(file_to_be_saved)
+                    cursor.execute(
+                        'UPDATE field_details SET details_image = %s WHERE detail_id = %s',
+                        (file_to_be_saved, field_details_id)
+                    )
+                    connection.commit()
+            return jsonify({'message': 'Field details added successfully'})
+        except mysql.connector.Error as e:
+            app.logger.error(f"Error adding field details: {str(e)}")
+            return jsonify({'error': 'Internal Server Error'}), 500
+        finally:
+            cursor.close()
+            connection.close()
 
 @app.route('/categories/<int:categoryID>', methods=['GET'])
 def get_category_fields(categoryID):
@@ -219,6 +258,36 @@ def manage_stocks():
         finally:
             cursor.close()
             connection.close()
+            
+@app.route('/dropdown/<path:text>', methods=['GET'])
+def get_dropdown_options(text):
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor(dictionary=True)
+        if text.isdigit():
+            cursor.execute('SELECT field_name FROM category_fields WHERE category_id = %s', (text,))
+            options = [result['field_name'] for result in cursor.fetchall()]
+        elif text == 'category':
+            cursor.execute('SELECT category_name FROM category')
+            options = [result['category_name'] for result in cursor.fetchall()]
+        elif text == 'category_fields':
+            cursor.execute('SELECT field_name FROM category_fields')
+            options = [result['field_name'] for result in cursor.fetchall()]
+        elif text.startswith('category_fields/'):
+            category_name = text.split('/')[1]
+            cursor.execute('SELECT category_id FROM category WHERE category_name = %s', (category_name,))
+            category_id = cursor.fetchone().get('category_id')
+            cursor.execute('SELECT field_name FROM category_fields WHERE category_id = %s', (category_id,))
+            options = [result['field_name'] for result in cursor.fetchall()]
+        else:
+            return jsonify({'error': 'Invalid text parameter'})
+        return jsonify(options)
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    finally:
+        cursor.close()
+        connection.close()
 
 @app.route('/uploads/<path:filename>', methods=['GET'])
 def uploaded_file(filename):
